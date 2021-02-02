67516e558cced4cd92f90dfcdf7fe55dc963ffc5
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-02-02 16:36:05 -0500

    edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
+
   devise_for :users, :controllers => {registrations: 'registrations'}
-  resources :breweries do
-    resources :ratings
+
+  resources :breweries, only: [:index] do
+    resources :ratings, only: [:index, :new, :create]
   end
-  
-  resources :ratings
+  resources :ratings, only: [:new, :create, :edit, :update, :destroy]
   resources :users
 
   root to: 'static#home'
   get '/about-us', to: 'static#about'
   get '/contact-us', to: 'static#contact'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
