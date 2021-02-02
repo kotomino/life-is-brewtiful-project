72bf6a1ccebf6a3989c121341d7988ffe39fed37
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-02-02 16:35:39 -0500

    removed nested attribute

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,11 +1,7 @@
 class Brewery < ApplicationRecord
   has_many :ratings
   has_many :users, through: :ratings
-
   validates :name, presence: true
-
-  accepts_nested_attributes_for :ratings, reject_if: :all_blank
-
   paginates_per 26
 
   def self.search(query)
