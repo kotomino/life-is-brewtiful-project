586437130a3c03f3ad4c1de6f4b15eba4c594619
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-02-01 21:41:54 -0500

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  resources :breweries
+  devise_for :users, :controllers => {registrations: 'registrations'}
+  resources :breweries do
+    resources :ratings
+  end
+  
   resources :ratings
   resources :users
+
+  root to: 'static#home'
+  get '/about-us', to: 'static#about'
+  get '/contact-us', to: 'static#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
