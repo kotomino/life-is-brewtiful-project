2e8bb3c9227b7cec90d50b1672a9c2d1656c16d8
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-02-02 16:33:53 -0500

    added validations

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,8 @@
 class Rating < ApplicationRecord
   belongs_to :user
   belongs_to :brewery
+  validates :rating, :inclusion => { :in => 1..5 }
+  validates_presence_of :rating, :comment
+
+  accepts_nested_attributes_for :brewery, reject_if: :all_blank
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :ratings
   has_many :breweries, through: :ratings
   validates_presence_of :email
