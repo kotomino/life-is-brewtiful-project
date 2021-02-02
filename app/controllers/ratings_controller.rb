54dba70854d727025d7219cae4e5e4e9e72b5bc9
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-02-02 16:32:53 -0500

    refactored ratings controller

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,34 +1,18 @@
 class RatingsController < ApplicationController
+
+  before_action :find_brewery, only: [:index, :new]
+  before_action :find_rating, only: [:edit, :update, :destroy]
+
   def index
-    # find_brewery
-    # if @brewery
-    #   @ratings = @brewery.ratings
-    #   @rating = @brewery.ratings.build
-    # else
-    #   @ratings = Rating.all
-    # end
-
-
-
-    if params[:brewery_id]
-      @brewery = Brewery.find_by(id: params[:brewery_id])
-      if @brewery.nil?
-        redirect_to breweries_path, alert: "Brewery not found"
-      else
-        @ratings = @brewery.ratings
-      end
+    #only nested route exists
+    if @brewery.nil?
+      redirect_to breweries_path, alert: "Brewery not found"
     else
-      @ratings = Rating.all
-    end
-  end
-
-  def show
-    @brewery = Brewery.find(params[:brewery_id])
-    @rating = @brewery.ratings.build
+      @ratings = @brewery.ratings
+    end 
   end
 
   def new
-    find_brewery
     if @brewery
       @rating = @brewery.ratings.build
       render :new_brewery_rating
@@ -38,14 +22,14 @@ class RatingsController < ApplicationController
   end
 
   def create
-
-    find_brewery
+    @brewery = Brewery.find_by_id(params[:rating][:brewery_id]) # find_brewery does not work; params hash has an extra layer
     @rating = Rating.new(rating_params)
+    @rating.user = current_user
     if @rating.save
       if @brewery
-        redirect_to brewery_path(@brewery)
+        redirect_to brewery_ratings_path(@brewery)
       else
-        redirect_to ratings_path 
+        redirect_to breweries_path
       end
     else
       flash.now[:error] = @rating.errors.full_messages
@@ -58,17 +42,28 @@ class RatingsController < ApplicationController
   end
 
   def edit
-    # @rating = Rating.find(params[:id])
-    # @brewery = Brewery.find(params[:rating_id])
   end
 
   def update
+    if @rating.update(rating_params)
+      redirect_to brewery_ratings_path(@rating.brewery_id)
+    else
+      flash.now[:error] = @movie.errors.full_messages
+      render :edit
+    end
   end
 
   def destroy
+    @rating.destroy
+    flash.now[:notice] = "Your review was removed."
+    redirect_to brewery_ratings_path(@rating.brewery_id)
   end
 
   private
+
+  def find_rating
+    @rating = Rating.find_by_id(params[:id])
+  end
 
   def find_brewery
     if params[:brewery_id]
