b7ad259046f1bbf6d272809972306df25a3fd03a
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-02-01 21:37:22 -0500

    added models

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,4 +1,19 @@
 class Brewery < ApplicationRecord
   has_many :ratings
   has_many :users, through: :ratings
+
+  validates :name, presence: true
+
+  accepts_nested_attributes_for :ratings, reject_if: :all_blank
+
+  paginates_per 26
+
+  def self.search(query)
+    where("LOWER(name) LIKE ? OR LOWER(city) LIKE ? OR LOWER(state) LIKE ?", "%#{query}%", "%#{query}%", "%#{query}%")
+  end
+
+  def self.alphabetize
+    order(:name)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :ratings
   has_many :breweries, through: :ratings
+  validates_presence_of :email
 end
