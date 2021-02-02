0e6f61a078f5385d89400009080abf631ac44db0
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-02-01 21:42:07 -0500

    db added

--- db/migrate/20210125161526_create_users.rb
+++ db/migrate/20210125161526_create_users.rb
@@ -1,8 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :email
-      t.string :password_digest
       t.string :first_name
       t.string :last_name
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Brewery.create(name: "Sweetwater", street: "123 Street", city: "Atlanta", state: "Georgia", postal_code: "30329", phone: "123-456-7890", website_url: "sweetwaterbrewery.com")
+
+# Api.load_breweries
+
+# Rating.create(rating: 1, comment: "Test review for Avondale Brewing Co.", brewery_id: 1, user_id: 1)
+Rating.create(rating: 5, comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.", brewery_id: 1, user_id: 1)
+
