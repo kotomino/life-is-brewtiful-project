3280026e9225ed24201babab06b0a2cdf7100379
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-02-02 16:33:18 -0500

    added user controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,21 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by_id(params[:user][:username])
+    if !@user
+      @error = "Username is incorrect"
+      render :new
+    elsif !user.authenticate(params[:user][:password])
+      @error = "Password is incorrect"
+      render :new
+    else
+      session[:user_id] = @user.id
+    end
+  end
+
+  
 end
