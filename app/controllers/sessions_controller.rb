634623c9c2475ea2955537dbf101a683e552fad0
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-02-04 22:39:52 -0500

    omniauth added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
 
   def new # get /login
+    redirect_if_logged_in
   end
 
   def create # post /login
@@ -19,4 +20,44 @@ class SessionsController < ApplicationController
     session.clear
     redirect_to root_path
   end
+
+  def google
+    @user = User.find_or_create_by(email: auth["email"], first_name: auth["first_name"], last_name: auth["last_name"]) do |user|
+      @user.password = SecureRandom.hex(10)
+    end
+    if @user && @user.id #if user exists and is saved 
+      login_user
+      redirect_to breweries_path
+    else
+      redirect_to signup_path
+    end
+  end
+
+  def facebook
+    @user = User.find_or_create_by(email: auth["email"], first_name: fb_first_name, last_name: fb_last_name) do |user|
+      @user.password = SecureRandom.hex(10)
+    end 
+    # binding.pry
+    if @user && @user.id #if user exists and is saved 
+      login_user
+      redirect_to breweries_path
+    else
+      redirect_to signup_path
+    end
+  end
+
+  private
+
+  def auth
+    request.env["omniauth.auth"]["info"]
+  end
+
+  def fb_first_name
+    auth["name"].split(" ").first
+  end
+
+  def fb_last_name
+    auth["name"].split(" ").last
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,22 @@ Rails.application.routes.draw do
   resources :breweries, only: [:index] do
     resources :ratings, only: [:index, :new, :create]
   end
-  resources :ratings, only: [:new, :create, :edit, :update, :destroy]
+  get "/top100", to: "breweries#top100", as: "top100"
 
+  resources :ratings, only: [:new, :create, :edit, :update, :destroy]
+  get "/my_reviews", to: "ratings#personal_index", as: "my_reviews"
+
+  get 'profile', to: "users#profile", as: "profile"
+
+  # Signup, Login, and Logout
   get "/signup", to: "users#new", as: "signup"
   post "/signup", to: "users#create"
-
   get "/login", to: "sessions#new", as: "login"
   post "/login", to: "sessions#create"
-
-
-  delete "logout", to: "sessions#destroy", as: "destroy_session"
+  delete "/logout", to: "sessions#destroy", as: "destroy_session"
   
+  match '/auth/google_oauth2/callback' => 'sessions#google', via: [:get, :post]
+  match '/auth/facebook/callback' => 'sessions#facebook', via: [:get, :post]
 
   root to: 'static#home'
   get '/about-us', to: 'static#about'
