0d58340dcb2740a865fa07cfb9f4dc9f99ade9d5
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-02-04 22:40:34 -0500

    rating average method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def rating_average(brewery)
+    if brewery.ratings.empty?
+      "~ / 5.0"
+    else
+      "%.1f" % brewery.ratings.average(:rating) + " / 5.0" #rounds to nearest tenth place in string form
+    end
+  end
+  
+  
 end
