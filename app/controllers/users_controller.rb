54e4670f9d2be91a1da50f653fc83544f73575c0
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-02-04 22:38:29 -0500

    redirect added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
-
+  
   def new
+    redirect_if_logged_in
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
+    
     if @user.save
       login_user
       redirect_to breweries_path
@@ -15,10 +17,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def profile
+    redirect_if_not_logged_in
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email, :first_name, :last_name, :password, :image)
   end
 
   
