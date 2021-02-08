bbb74f4cb5bae47f2426300777e86cb69243a056
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-02-07 19:30:04 -0500

    phone number styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,19 @@
 module ApplicationHelper
+
   def rating_average(brewery)
     if brewery.ratings.empty?
       "~ / 5.0"
     else
       "%.1f" % brewery.ratings.average(:rating) + " / 5.0" #rounds to nearest tenth place in string form
+    end
+  end
+
+  def style(phone_number) #styles phone number to include dashes
+    if phone_number != ""
+      array = phone_number.split("")
+      array[0..2].join + "-" + array[3..5].join + "-" + array[6..9].join
+    else
+      "N/A"
     end
   end
   
