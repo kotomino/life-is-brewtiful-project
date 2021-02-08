22fad68f814a5c5fbb27b4487715753752ab08c3
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-02-07 19:33:38 -0500

    cleaned up seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,1 @@
-
-
-
 Api.load_breweries
-
-# # Rating.create(rating: 1, comment: "Test review for Avondale Brewing Co.", brewery_id: 1, user_id: 1)
-# Rating.create(rating: 5, comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.", brewery_id: 1, user_id: 1)
-
