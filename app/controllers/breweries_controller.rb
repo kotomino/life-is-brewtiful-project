c0302f6b1a12be6ed26dfa8b1fc851304b64a116
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-02-16 13:39:57 -0500

    added comments

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,9 +1,9 @@
 class BreweriesController < ApplicationController
 
   def index
-    if params[:query]
+    if params[:query] #if search field is filled in, show results only
       @breweries = Brewery.search(params[:query]).page(params[:page])
-    else
+    else #no search; show all breweries
       @breweries = Brewery.all.page(params[:page])
     end
   end
@@ -14,7 +14,7 @@ class BreweriesController < ApplicationController
 
   private
 
-  def brewery_params
+  def brewery_params #not used currently, but may need if adding additional routes in the future.
     params.require(:brewery).permit(
       :name, :street, :city, :state, :postal_code, :phone, :website_url,
       ratings_attributes: [:rating, :comment]
